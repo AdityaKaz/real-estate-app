6c79287ea7b4a3cd029e9e5bfa56d6e893a72e17
= <=>
2025-08-16 16:35:10 +0530

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,21 @@
-import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
-const app = express();
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+dotenv.config({ path: "./.env" });
+const port = process.env.PORT || 3000;
+
+app.on("error", (error) => {
+  console.log("Error: ", error);
+  throw error;
 });
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB connection Failed !", error);
+  });
