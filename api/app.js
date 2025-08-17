c2f6774543312183fbcd2662a6296d3a3e62c693
= <=>
2025-08-17 18:43:53 +0530

    created middleware and function to handle errors

--- api/app.js
+++ api/app.js
@@ -8,5 +8,10 @@ app.use(express.json());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.status || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({ success: false, statusCode, message });
+});
 
 export { app };
