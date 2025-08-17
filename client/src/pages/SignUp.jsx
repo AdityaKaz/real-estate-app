169546405bfddea7b96d7ad7d673d82250f23992
= <=>
2025-08-17 19:22:48 +0530

    completed sign-up page UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,45 @@
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
-  return <div>Sign Up Page</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Username"
+          className="border border-slate-300 p-3 rounded-lg"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="border border-slate-300 p-3 rounded-lg"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border border-slate-300 p-3 rounded-lg"
+          id="password"
+        />
+        <button
+          type="submit"
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80"
+        >
+          Sign Up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>
+          Have an account?{" "}
+          <Link to="/sign-in" className="text-blue-700">
+            Sign In
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
