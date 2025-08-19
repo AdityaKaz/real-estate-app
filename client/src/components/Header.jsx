a178928229218dc2a5c72057b5d1ad930efde655
= <=>
2025-08-19 22:13:04 +0530

    made changes to Header UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,13 @@
 import NestIcon from "../assets/nest_icon.png";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation();
+
+  const hidePaths = ["/sign-in", "/sign-up"];
+  const shouldHideElements = hidePaths.includes(location.pathname);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div
@@ -13,7 +18,7 @@ const Header = () => {
           <img
             src={NestIcon}
             alt="Nest Logo"
-            className="h-5 w-5 mr-1 sm:h-6 sm:w-6 lg:h-9 lg:w-9 lg:mr-2"
+            className="h-5 w-5 mr-0.5 sm:h-6 sm:w-6 lg:h-9 lg:w-9 lg:mr-1"
           />
           <h1 className="font-extrabold text-base sm:text-lg lg:text-2xl">
             <span className="text-slate-800">Nest</span>
@@ -21,34 +26,38 @@ const Header = () => {
           </h1>
         </Link>
 
-        <form className="justify-self-center w-full max-w-[120px] sm:max-w-[200px] md:max-w-[300px] lg:max-w-[400px] xl:max-w-[600px]">
-          <div className="flex items-center bg-slate-100 p-1 rounded-lg shadow">
-            <input
-              type="text"
-              placeholder="Search..."
-              className="bg-transparent focus:outline-none w-full text-sm lg:text-base"
-            />
-            <FaSearch className="text-slate-600 ml-1 text-base lg:text-xl" />
-          </div>
-        </form>
+        {!shouldHideElements && (
+          <form className="justify-self-center w-full max-w-[120px] sm:max-w-[200px] md:max-w-[300px] lg:max-w-[400px] xl:max-w-[600px]">
+            <div className="flex items-center bg-slate-100 p-1 rounded-lg shadow">
+              <input
+                type="text"
+                placeholder="Search..."
+                className="bg-transparent focus:outline-none w-full text-sm lg:text-base"
+              />
+              <FaSearch className="text-slate-600 ml-1 text-base lg:text-xl" />
+            </div>
+          </form>
+        )}
 
-        <ul className="flex gap-2 text-xs sm:text-sm lg:gap-4 lg:text-base justify-self-end">
-          <Link to="/">
-            <li className="hidden sm:inline text-slate-600 hover:text-slate-900 cursor-pointer font-bold">
-              Home
-            </li>
-          </Link>
-          <Link to="/about">
-            <li className="hidden sm:inline text-slate-600 hover:text-slate-900 cursor-pointer font-bold">
-              About
-            </li>
-          </Link>
-          <Link to="/sign-in">
-            <li className="text-slate-600 hover:text-slate-900 cursor-pointer font-bold">
-              Sign-in
-            </li>
-          </Link>
-        </ul>
+        {!shouldHideElements && (
+          <ul className="flex gap-2 text-xs sm:text-sm lg:gap-4 lg:text-base justify-self-end">
+            <Link to="/">
+              <li className="hidden sm:inline text-slate-600 hover:text-slate-900 cursor-pointer font-bold">
+                Home
+              </li>
+            </Link>
+            <Link to="/about">
+              <li className="hidden sm:inline text-slate-600 hover:text-slate-900 cursor-pointer font-bold">
+                About
+              </li>
+            </Link>
+            <Link to="/sign-in">
+              <li className="text-slate-600 hover:text-slate-900 cursor-pointer font-bold">
+                Sign-in
+              </li>
+            </Link>
+          </ul>
+        )}
       </div>
     </header>
   );
